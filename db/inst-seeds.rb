6752681b58d5b5aac1e37265bd364805d1772e23
dweebit <brakit@gmail.com>
2012-04-14 15:00:07 -0700

    Add bay area institutional seed data

---
+++ db/inst-seeds.rb
@@ -0,0 +1,9 @@
+ # Thing.create(:city_id => 1, :lng => -71.049226, :lat => 42.383339)
+Thing.create(:city_id => 1, :lng => -122.165837, :lat => 37.425937) # Stanford
+Thing.create(:city_id => 2, :lng => -122.245831, :lat => 37.87526) # UC Berkeley
+Thing.create(:city_id => 3, :lng => -121.75694, :lat => 38.544406) # UC Davis
+Thing.create(:city_id => 4, :lng => -122.457842, :lat => 37.763065) # UC SF
+Thing.create(:city_id => 5, :lng => -122.058603, :lat => 36.990265) # UC SC
+Thing.create(:city_id => 6, :lng => -122.479663, :lat => 37.723725) #SF State
+
+
