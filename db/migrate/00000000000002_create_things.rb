14e137d72e663ad56b7967ba9db4c6200dd0f786
Ben Sheldon <bensheldon@gmail.com>
2012-04-18 15:38:11 -0700

    Changed up models and seeds to add actually institutional names.

--- db/migrate/00000000000002_create_things.rb
+++ db/migrate/00000000000002_create_things.rb
@@ -5,8 +5,6 @@ class CreateThings < ActiveRecord::Migration
       t.string :name
       t.decimal :lat, :null => false, :precision => 16, :scale => 14
       t.decimal :lng, :null => false, :precision => 17, :scale => 14
-      t.integer :city_id
-      t.integer :user_id
     end
 
     add_index :things, :city_id, :unique => true
